4ed99068468d837faa0e9a00c2d77da20a922bd6
Sharad Singh <ofcsharadsinghthakur@gmail.com>
2025-08-06 15:52:55 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,6 @@ export default function Home() {
         <EducationSection />
         <ExperienceSection />
         <SkillsSection />
-        <ProjectsSection />
         <ContactSection />
       </main>
       <Footer />
